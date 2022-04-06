0545c84525cd11572be6a04bec08bc5671d81782
kimingang <sofargang@gmail.com>
2022-04-06 17:31:34 +0900

    01_cpp_coding_style.cpp

--- 00_init.cpp
+++
@@ -1,8 +0,0 @@
-/* 00_init */
-#include<iostream>
-using namespace std;
-
-int main(void){
-	cout << "hello world" << endl;
-	return 0;
-}
--- 01_cpp_coding_style.cpp
+++ 01_cpp_coding_style.cpp
@@ -31,7 +31,75 @@ struct DataSetType{
 private:
   int nAccessCount_;
 };
-class FODeleteElement{
+class FODeleteElement{#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <typeinfo>
+
+using namespace std;
+
+#ifdef _DEBUG
+#define SAMPLE_D_FUNC_INFO   cout << __FUNCTION__ << ':' << __LINE__ << ':' << __DATE__ << ' ' << __TIME__ << endl
+#else
+#define SAMPLE_D_FUNC_INFO
+#endif
+
+template <typename Container>
+size_t fnPrintContainer(const Container& c, const char* str = "")
+{
+    SAMPLE_D_FUNC_INFO;
+    cout << str << ':';
+    for (const auto& val : c) {
+        cout << val << ' ';
+    }
+    cout << endl;
+    return c.size();
+}
+
+class DataType {
+public:
+    //! defalut constructor
+    explicit DataType(int n = 0, const string& str = "") : nUserID_(n), strUserName_(str) {}
+    //! member variables to string
+    const string to_string() const
+    {
+        ostringstream oss;
+        oss << nUserID_ << ',' << strUserName_;
+        return oss.str();
+    }
+
+private:
+    int nUserID_;       ///< User's ID
+    string strUserName_;///< User's name
+};
+
+// operator<< overload for putting the DataType to ostream
+std::ostream& operator<<(ostream& os, const DataType& data)
+{
+    os << data.to_string();
+    return os;
+}
+
+int main()
+{
+    // list of int
+    list<int> listInt = { 0,1,2,3,4,5,6,7,8,9 };
+    // vector of int
+    vector<int> vecInt = { 0,1,2,3,4,5,6,7,8,9 };
+    // vector of DataType
+    vector<DataType> vecData;
+    vecData.emplace_back(1, "Steve");
+    vecData.emplace_back(2, "Robert");
+    vecData.emplace_back(3, "Michael");
+
+    fnPrintContainer(listInt, typeid(listInt).name());
+    fnPrintContainer(vecInt, typeid(vecInt).name());
+    fnPrintContainer(vecData, typeid(vecData).name());
+
+    return EXIT_SUCCESS;
+}
 public:
     void delete()(Object* obj);
 }
@@ -42,38 +110,42 @@ public:
   enum enumDATA_TYPE { eINT, eDOUBLE } // enumeration
 
 - variable prefix
-prefix	description
-gv	    global variable
-p	    pointer type
-c	    signed char
-uc	    unsigned char
-wc	    wide char
-s	    signed short
-us	    unsigned short
-n	    signed integer
-un	    unsigned integer
-l	    signed long
-ul	    unsigned long
-ll	    signed long long
-f	    sigle floating number
-d	    double floating number
-vec	    vector
-list	list
-deq	    deque
-map	    map
-mmap	multi map
-set	    set
-mset	multi set
-umap	unordered map
-ummap	unordered multi map
-uset	unordered set
-umset	unordered multi set
-sz	    char array, include null
-wsz	    wide char array, include null
-str	    std::string
-wstr	std::wstring
+prefix		description
+gv			global variable
+p			pointer type
+c			signed char
+uc			unsigned char
+wc			wide char
+s			signed short
+us			unsigned short
+n			signed integer
+un			unsigned integer
+l			signed long
+ul			unsigned long
+ll			signed long long
+f			sigle floating number
+d			double floating number
+vec			vector
+list		list
+deq			deque
+map			map
+mmap		multi map
+set			set
+mset		multi set
+umap		unordered map
+ummap		unordered multi map
+uset		unordered set
+umset		unordered multi set
+sz			char array, include null
+wsz			wide char array, include null
+str			std::string
+wstr		std::wstring
 
+- Comments use C++ basic format for full explanation, and detailed comments in code use Doxygen format.
+///<		: to be syntax + comment same line  : while(n < 0) ///< number count loop
+/*!			: single or multi line comments
 */
+//!			: single line comment
 
 #include <iostream>
 #include <sstream>
